2750e5d2243b759180dc1cc3982126c8b878dd95
virshu <felix@rabinovich.org>
2026-01-19 14:06:54 -0800

    Contry New and Edit

--- src/app/countries/countries.service.ts
+++ src/app/countries/countries.service.ts
@@ -1,4 +1,4 @@
-import { httpResource } from '@angular/common/http';
+import { HttpClient, httpResource } from '@angular/common/http';
 import { effect, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Country } from './country';
@@ -12,12 +12,36 @@ export class CountriesService {
   readonly countries = this.resource.value;
   readonly error = this.resource.error;
 
-   constructor() { 
+  constructor(private http: HttpClient) { 
     effect(() => {
       if (this.error()) {
         console.error('Error loading countries:', this.error());
       }
     });
+  }
+
+  updateCountry(country: Country): void {
+    if (country.id === 0) {
+      this.http.post<Country>(`${environment.baseUrl}api/countries`, country).subscribe({
+        next: (createdCountry) => {
+          console.log('Country created successfully:', createdCountry);
+        },
+        error: (error) => {
+          console.error('Error creating country:', error);
+        }
+      });
+    } else {
+      // Implementation for updating a country
+      this.http.put<Country>(`${environment.baseUrl}api/countries/${country.id}`, country).subscribe({
+        next: (updatedCountry) => {
+          console.log('Country updated successfully:', updatedCountry);
+        },
+        error: (error) => {
+          console.error('Error updating country:', error);
+        }
+      });
+      console.log('Updating country:', country);
+    }
   }
 
 }
--- src/app/countries/country-edit.component.ts
+++ src/app/countries/country-edit.component.ts
@@ -1,18 +1,21 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { Country } from './country';
-import { form, FormField } from '@angular/forms/signals';
-import { ActivatedRoute } from '@angular/router';
+import { form, FormField, hidden, maxLength, minLength, submit } from '@angular/forms/signals';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { httpResource } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { CountriesService } from './countries.service';
 
 @Component({
   selector: 'app-country-edit',
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    FormField],
+    FormField,
+    RouterLink
+  ],
   templateUrl: './country-edit.component.html',
   styleUrl: './country-edit.component.scss',
 })
@@ -23,19 +26,35 @@ export class CountryEditComponent {
     iso2: '',
     iso3: ''
   });
-  countryForm = form(this.countryModel);
+  countryForm = form(this.countryModel, (country) => {
+    minLength(country.iso2, 2);
+    maxLength(country.iso2, 2);
+    minLength(country.iso3, 3);
+    maxLength(country.iso3, 3);
+    hidden(country.id, () => true);
+  });
+
+  private readonly countryService = inject(CountriesService);
 
     constructor(private activatedRoute: ActivatedRoute) {
-    let idParam = this.activatedRoute.snapshot.paramMap.get('id');
-    let countryId = idParam ? +idParam : -1;
-    let resource = httpResource<Country>(() => 
-      `${environment.baseUrl}api/countries/${countryId}`);
-    // https://stackoverflow.com/questions/79850289/angular-signal-forms-with-resources
-    effect(() => {
-      if (resource.hasValue()) {
-        this.countryModel.set(resource.value());
+      let idParam = this.activatedRoute.snapshot.paramMap.get('id');
+      let countryId = idParam ? +idParam : 0;
+      if (countryId > 0) {
+        let resource = httpResource<Country>(() => 
+          `${environment.baseUrl}api/countries/${countryId}`);
+        // https://stackoverflow.com/questions/79850289/angular-signal-forms-with-resources
+        effect(() => {
+          if (resource.hasValue()) {
+            this.countryModel.set(resource.value());
+          }
+        });
       }
-    });
-  }
+    }
 
+    onSubmit(event: Event): void {
+      event.preventDefault();
+      if (this.countryForm().valid()) {
+        this.countryService.updateCountry(this.countryModel());
+      }
+    }
 }
